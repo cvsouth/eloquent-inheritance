5e6514908632990956a4811e7093545fc216d3d1
Colin South <colin@riseresolution.com>
2019-08-25 12:18:36 +0300

    ...

--- src/Migrations/1970_01_01_000000_create_base_models_table.php
+++ src/Migrations/1970_01_01_000000_create_base_models_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Schema\Blueprint;
 
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInheritableModelsTable extends Migration
+class CreateBaseModelsTable extends Migration
 {
     public function up()
     {
